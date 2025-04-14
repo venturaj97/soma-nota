ffefbb984523073ed45f3705d2fa15324d757443
venturaj97 <joaoventura97@outlook.com>
2025-04-14 15:37:12 -0300

    logica para processar imagem

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
-
+import cv2 # OpenCV
+import numpy as np
 
 
 st.title("Sistema soma-nota")
@@ -12,3 +13,16 @@ if uploaded_file is not None:
     # Mostrar a imagem carregada
     image = Image.open(uploaded_file)
     st.image(image, caption='Imagem Carregada', use_container_width=True)
+
+    if st.button("Processar Imagem"):
+        with st.spinner('Processando...'):
+
+            img_cv = np.array(image)
+            img_cv = cv2.cvtColor(img_cv, cv2.COLOR_RGB2BGR) 
+
+            # 1. Pré-processamento (Exemplo: escala de cinza) - Refinar depois!
+            gray_image = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
+            processed_image = gray_image # Começar simples
+
+
+            st.image(img_cv, caption='Imagem Processada', use_container_width=True)
